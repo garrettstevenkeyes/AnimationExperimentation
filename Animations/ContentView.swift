120f0cb6dd269474f3bab089da059553dae52a11
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-16 15:46:52 -0500

    Word snake

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,22 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var letters = Array("Swift Testing")
+    @State private var enabled: Bool = false
     @State private var dragAmount = CGSize.zero
 
     var body: some View {
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
+        HStack(spacing:0){
+            ForEach(0..<letters.count, id: \.self) {num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
                 DragGesture()
-                    .onChanged {dragAmount = $0.translation }
+                    .onChanged { dragAmount = $0.translation }
                     .onEnded { _ in
-                        withAnimation(.bouncy) {
-                            dragAmount = .zero
-                        }
+                        dragAmount = .zero
+                        enabled.toggle()
                     }
-            )
+        )
     }
 }
 
