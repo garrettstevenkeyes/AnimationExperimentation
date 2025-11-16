367be4b5057407936ad89cd6743b78f4292704a8
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-16 13:57:56 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Garrett Keyes on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
